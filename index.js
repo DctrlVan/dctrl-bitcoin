e69c0af3a6c6179202e607ee166550963ce0f862
taylor <taylorsingletonfookes@live.com>
2017-06-28 16:15:21 -0700

    streamsNrpcs

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+const Kefir = require('kefir')
+const bitcoin = require('bitcoinjs-lib')
+const bitcoind = require('bitcoin')
 
-var zmq = require('zmq')
-  , sock = zmq.socket('sub');
+// all config options are optional
+const client = new bitcoind.Client({
+  host: 'localhost',
+  port: 8332,
+  user: 'bitcoin',
+  pass: 'local321'
+});
 
-sock.subscribe("hashblock");
-sock.subscribe("hashtx");
+client.cmd('getbalance', '*', 6, function(err, balance, resHeaders){
+  if (err) return console.log(err);
+  console.log('Balance:', balance);
+});
+
+
+var blkEmitter, txEmitter
+const blkStream = Kefir.stream( e => {
+    blockEmitter = e
+}).log('block party!')
+
+const txStream = Kefir.stream( e => {
+    txEmitter = e
+})
+.onValue( tx => {
+    let id = tx.getId()
+    console.log({id})
+})
+const sock = require('zmq').socket('sub')
+// sock.subscribe("hashblock");
+// sock.subscribe("hashtx");
 sock.subscribe("rawblock");
 sock.subscribe("rawtx");
 
 sock.on('message', function(topic, message) {
     switch ( topic.toString('utf8') ) {
+        case 'rawblock':
+            const blk = bitcoin.Block.fromBuffer(message)
+            blockEmitter.emit(blk)
+            break
         case 'rawtx':
-            console.log('in rawtx handler')
+            const tx = bitcoin.Transaction.fromBuffer(message)
+            txEmitter.emit(tx)
             break
         case 'hashtx':
             console.log('in hashtx handler')
-            break
-        case 'rawblock':
-            console.log('in rawblock handler')
             break
         case 'hashblock':
             console.log('in hashblock handler')
@@ -27,6 +56,23 @@ sock.on('message', function(topic, message) {
 sock.on('connect', function(fd, ep) {console.log('connect, endpoint:', ep);});
 
 sock.connect('tcp://127.0.0.1:28332');
+
+function handleBlock(block){
+    console.log('in rawblock handler')
+    block.transactions.forEach( tx => {
+        console.log(tx)
+        tx.outs.forEach( output => {
+            console.log({
+              output: output.toString('utf8')})
+
+        })
+    })
+}
+
+function handleTx(tx){
+    console.log('handling tx')
+
+}
 
 //
 // // subber.js
